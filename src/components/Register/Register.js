5f537f9716f42e9b5625dc6fd4163e043c85c4c0
Guilherme Alonso <guilherme.malonso@gmail.com>
2018-08-25 13:19:13 -0300

    Database connection adjustments

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -41,7 +41,6 @@ class Register extends React.Component {
   }
 
   render() {
-    const { onRouteChange } = this.props;
     return (
       <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
@@ -80,7 +79,7 @@ class Register extends React.Component {
               </div>
             </fieldset>
             <div className="">
-              <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
+              <input onClick={this.onSubmitSignIn} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Register" />
             </div>
           </div>
         </main>
